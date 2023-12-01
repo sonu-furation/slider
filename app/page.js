87043d155f4918ab5e0eee7b1a455d3aebe970da
sonu kumar <sonu.kumar@furation.tech>
2023-12-01 18:35:30 +0530

    demo

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Slider from "@/components/slider";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <Slider />
+    </main>
+  );
+}
---
+++ components/slider.jsx
@@ -0,0 +1,42 @@
+"use client";
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "./styles.css"; // Import your custom styles
+import { Navigation } from "swiper/modules";
+
+export default function App() {
+  return (
+    <>
+      <Swiper
+        slidesPerView={1}
+        loop={true}
+        navigation={true}
+        modules={[Navigation]}
+        className="mySwiper"
+        breakpoints={{
+          400: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+        }}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
